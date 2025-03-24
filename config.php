7a4eff74a85cae79bb06c3dfe27e7c53270c105d
Lucas Matheus da Cruz <lucas.matheus@fique.online>
2025-03-24 20:33:14 -0300

    refactor(api, config): update naming conventions and connection settings
    - Rename field 'Principio_Ativo' to 'PrincipioAtivo' in get_services.php to ensure query consistency.
    - Update database configuration in config.php: change host to 191.252.1.143 and adjust port to 3306.

--- config.php
+++ config.php
@@ -1,26 +1,23 @@
 <?php
-$host = "localhost";
-$user = "douglas";  
-$pass = "Douglas193";      
-$dbname = "bd_servico"; 
-$port = "3307";
 
-// Configurar modo de erro para mysqli
+$host = "191.252.1.143";           
+$user = "douglas";      
+$pass = "Douglas193";   
+$dbname = "bd_servico"; 
+$port = 3306;           
+
+
+
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-try {
-    // Criando conexão com o banco de dados
-    $conn = new mysqli($host, $user, $pass, $dbname);
-    
-    // Verificando a conexão
+try { 
+    $conn = new mysqli($host, $user, $pass, $dbname, $port);
     if ($conn->connect_error) {
         throw new Exception("Falha na conexão: " . $conn->connect_error);
     }
 } catch (Exception $e) {
-    // Logar o erro
     error_log("Erro de conexão com o banco: " . $e->getMessage());
     
-    // Retornar erro como JSON
     header("Content-Type: application/json");
     echo json_encode(["error" => "Falha na conexão com o banco", "details" => $e->getMessage()]);
     exit;
